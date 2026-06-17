dfe2f774169ef92a39d1944accc10a3be31db110
Nusaiba Zafnah <nusaibazafnah.nz@gmail.com>
2026-06-17 17:04:44 +0600

    Added GPA tests

--- test.sh
+++ test.sh
@@ -6,3 +6,7 @@ chmod +x test_course.out
 gcc -I Module tests/test_course_result.c Module/course.c Module/courseResult.c -o test_course_result.out
 chmod +x test_course_result.out
 ./test_course_result.out
+
+gcc -I Module tests/test_gpa.c Module/course.c Module/courseResult.c Module/gpa.c -o test_gpa.out
+chmod +x test_gpa.out
+./test_gpa.out
