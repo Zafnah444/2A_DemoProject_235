8535da9c98f1275e70e4284c14228290e5a0c3e1
Nusaiba Zafnah <nusaibazafnah.nz@gmail.com>
2026-06-18 17:34:07 +0600

    Added tests for semester course helpers

--- test.sh
+++ test.sh
@@ -2,11 +2,11 @@
 gcc -I Module tests/test_course.c Module/course.c -o test_course.out
 chmod +x test_course.out
 ./test_course.out
-
-gcc -I Module tests/test_course_result.c Module/course.c Module/courseResult.c -o test_course_result.out
+gcc -I Module tests/test_course_result.c Module/course.c Module/courseResult.c -o
+test_course_result.out
 chmod +x test_course_result.out
 ./test_course_result.out
-
-gcc -I Module tests/test_gpa.c Module/course.c Module/courseResult.c Module/gpa.c -o test_gpa.out
+gcc -I Module tests/test_gpa.c Module/course.c Module/courseResult.c Module/gpa.c -o
+test_gpa.out
 chmod +x test_gpa.out
 ./test_gpa.out
